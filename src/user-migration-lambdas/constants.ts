79cc4eb16f45db90c02dd58239a298d53bd085e7
ganupam05201993 <75247399+ganupam05201993@users.noreply.github.com>
2023-03-10 00:20:55 +0530

    Create constants.ts

---
+++ src/user-migration-lambdas/constants.ts
@@ -0,0 +1,5 @@
+export const MONGODB_URI  =  process.env.MONGO_URL?  process.env.MONGO_URL : "";
+export const INCORRECT_PWD = "Incorrect Password Entered";
+export const INCORRECT_USER = "does not exist in Wave";
+export const INCORRECT_TRIGGER = "Bad Trigger";
+export const INCORRECT_USER_DOMAIN= "Swell Energy users must sign in through Google";
