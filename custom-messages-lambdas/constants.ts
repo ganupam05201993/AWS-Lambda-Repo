7e06884e9886bbe6da9018d40c5d0a7c0b48298f
ganupam05201993 <75247399+ganupam05201993@users.noreply.github.com>
2023-03-09 23:44:57 +0530

    Delete constants.ts

--- custom-messages-lambdas/constants.ts
+++
@@ -1,5 +0,0 @@
-export const MONGODB_URI  =  process.env.MONGO_URL!;
-export const MAILGUN_KEY = process.env.MAILGUN_KEY!;
-export const MAILGUN_DOMAIN = process.env.MAILGUN_DOMAIN!;
-export const INCORRECT_TRIGGER = "Bad Trigger";
-export const TEMPLATE_TYPE =process.env.TEMPLATE_TYPE!;
